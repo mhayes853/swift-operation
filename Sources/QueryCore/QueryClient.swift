8a7a126f3848b7f6c594e05320df4fb5f95fb000
mhayes853 <matthewha853@gmail.com>
2025-02-24 00:56:55 -0800

    Runtime warn if same QueryPath is used for 2 different QueryProtocol conformances

--- Sources/QueryCore/QueryClient.swift
+++ Sources/QueryCore/QueryClient.swift
@@ -1,7 +1,9 @@
+import IssueReporting
+
 // MARK: - QueryClient
 
 public final class QueryClient: Sendable {
-  private let stores = Lock<[QueryPath: Any]>([:])
+  private let stores = Lock<[QueryPath: StoreEntry]>([:])
 
   public init() {}
 }
@@ -10,26 +12,63 @@ public final class QueryClient: Sendable {
 
 extension QueryClient {
   public func store<Query: QueryProtocol>(for query: Query) -> QueryStore<Query.Value?> {
-    self.stores.withLock { stores in
-      if let store = stores[query.path] as? QueryStateStore<Query.Value?> {
-        return QueryStore(query: query, state: store)
-      }
-      let store = QueryStateStore<Query.Value?>(initialValue: nil)
-      stores[query.path] = store
-      return QueryStore(query: query, state: store)
-    }
+    QueryStore(query: query, state: self.queryStateStore(for: query, initialValue: nil))
   }
 
   public func store<Query: QueryProtocol>(
     for query: DefaultQuery<Query>
   ) -> QueryStore<Query.Value> {
+    QueryStore(
+      query: query,
+      state: self.queryStateStore(for: query, initialValue: query.defaultValue)
+    )
+  }
+
+  private func queryStateStore<Query: QueryProtocol>(
+    for query: Query,
+    initialValue: Query.Value?
+  ) -> QueryStateStore<Query.Value?> {
     self.stores.withLock { stores in
-      if let store = stores[query.path] as? QueryStateStore<Query.Value?> {
-        return QueryStore(query: query, state: store)
+      if let entry = stores[query.path],
+        let store = entry.stateStore as? QueryStateStore<Query.Value?>
+      {
+        if entry.queryType != Query.self {
+          duplicatePathWarning(expectedType: entry.queryType, foundType: Query.self)
+        }
+        return store
       }
-      let store = QueryStateStore<Query.Value?>(initialValue: query.defaultValue)
-      stores[query.path] = store
-      return QueryStore(query: query, state: store)
+      let store = QueryStateStore<Query.Value?>(initialValue: initialValue)
+      stores[query.path] = StoreEntry(queryType: Query.self, stateStore: store)
+      return store
     }
   }
+}
+
+// MARK: - Store Entry
+
+extension QueryClient {
+  private struct StoreEntry {
+    let queryType: Any.Type
+    let stateStore: Any
+  }
+}
+
+// MARK: - Warning
+
+private func duplicatePathWarning(expectedType: Any.Type, foundType: Any.Type) {
+  reportIssue(
+    """
+    A QueryClient has detected a duplicate QueryPath used for different QueryProtocol conformances.
+
+        Expected: \(String(reflecting: expectedType))
+           Found: \(String(reflecting: foundType))
+
+    The returned QueryStore instance will use the expected QueryProtocol conformance type for \
+    data fetching.
+
+    To fix this, ensure that all of your QueryProtocol conformances return unique QueryPath \
+    instances. If your QueryProtocol conformance type conforms to Hashable, the default QueryPath \
+    is represented by a single element path containing the instance of the type itself.
+    """
+  )
 }
--- Tests/QueryCoreTests/QueryClientTests.swift
+++ Tests/QueryCoreTests/QueryClientTests.swift
@@ -1,4 +1,5 @@
 import CustomDump
+import IssueReporting
 import QueryCore
 import Testing
 
@@ -15,19 +16,13 @@ struct QueryClientTests {
     expectNoDifference(store2.value, TestQuery.value)
   }
 
-  @Test(
-    "Maintains The Same Query State For Multiple Stores With The Same Query With Default Values"
-  )
-  func maintainsValueForMultipleStoresWithDefaultValues() async throws {
+  @Test("Reports Issue When Different Query Type Has The Same Path As Another Query")
+  func cannotHaveDuplicatePaths() async throws {
     let client = QueryClient()
-    let store1 = client.store(for: TestQuery())
-    try await store1.fetch()
-    let store2 = client.store(for: TestQuery().defaultValue(TestQuery.value + 10))
-
-    expectNoDifference(store1.value, store2.value)
-    expectNoDifference(store1.defaultValue, nil)
-    expectNoDifference(store2.defaultValue, TestQuery.value + 10)
-    expectNoDifference(store2.value, TestQuery.value)
+    _ = client.store(for: TestQuery())
+    withExpectedIssue {
+      _ = client.store(for: TestQuery().defaultValue(TestQuery.value + 10))
+    }
   }
 
   @Test("Does Not Share States Between Different Queries")
