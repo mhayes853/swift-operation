108e6df53028f9df6f9a802adfbc850addfc4cb0
mhayes853 <matthewha853@gmail.com>
2025-03-06 10:35:13 -0800

    MutationState type

---
+++ Sources/QueryCore/Mutations/MutationState.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+// MARK: - MutationState
+
+public struct MutationState<StateValue: Sendable, QueryValue: Sendable> {
+
+}
+
+// MARK: - QueryStateProtocol
+
+extension MutationState: QueryStateProtocol {
+  public typealias StatusValue = QueryValue
+
+  public var currentValue: StateValue {
+    fatalError()
+  }
+
+  public var initialValue: StateValue {
+    fatalError()
+  }
+
+  public var valueUpdateCount: Int {
+    fatalError()
+  }
+
+  public var valueLastUpdatedAt: Date? {
+    fatalError()
+  }
+
+  public var isLoading: Bool {
+    fatalError()
+  }
+
+  public var error: (any Error)? {
+    fatalError()
+  }
+
+  public var errorUpdateCount: Int {
+    fatalError()
+  }
+
+  public var errorLastUpdatedAt: Date? {
+    fatalError()
+  }
+
+  public mutating func startFetchTask(
+    in context: QueryContext,
+    for fn: @escaping @Sendable () async throws -> any Sendable
+  ) -> Task<any Sendable, any Error> {
+    fatalError()
+  }
+
+  public mutating func endFetchTask(
+    in context: QueryContext,
+    with result: Result<QueryValue, any Error>
+  ) {
+    fatalError()
+  }
+}
