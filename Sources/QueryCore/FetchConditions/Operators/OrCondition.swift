1a866c9de27aebb6d9f1e9c24ca334ae2274bb04
mhayes853 <matthewha853@gmail.com>
2025-03-12 12:27:06 -0700

    OrCondition

--- Sources/QueryCore/FetchConditions/Operators/OrCondition.swift
+++ Sources/QueryCore/FetchConditions/Operators/OrCondition.swift
@@ -5,14 +5,32 @@ public struct OrCondition<Left: FetchCondition, Right: FetchCondition> {
 
 extension OrCondition: FetchCondition {
   public func isSatisfied(in context: QueryContext) -> Bool {
-    true
+    self.left.isSatisfied(in: context) || self.right.isSatisfied(in: context)
   }
 
   public func subscribe(
     in context: QueryContext,
     _ observer: @escaping @Sendable (Bool) -> Void
   ) -> QuerySubscription {
-    .empty
+    let state = Lock(
+      (left: self.left.isSatisfied(in: context), right: self.right.isSatisfied(in: context))
+    )
+    let s1 = self.left.subscribe(in: context) { value in
+      state.withLock {
+        $0.left = value
+        observer($0.left || $0.right)
+      }
+    }
+    let s2 = self.right.subscribe(in: context) { value in
+      state.withLock {
+        $0.right = value
+        observer($0.left || $0.right)
+      }
+    }
+    return QuerySubscription {
+      s1.cancel()
+      s2.cancel()
+    }
   }
 }
 
--- Tests/QueryCoreTests/FetchConditionsTests/OperatorTests/OrConditionTests.swift
+++ Tests/QueryCoreTests/FetchConditionsTests/OperatorTests/OrConditionTests.swift
@@ -2,42 +2,42 @@ import CustomDump
 import QueryCore
 import Testing
 
-@Suite("AndCondition tests")
-struct AndConditionTests {
+@Suite("OrCondition tests")
+struct OrConditionTests {
   @Test("Is True When Both Conditions Are True")
   func trueWhenBothTrue() {
     let c1: some FetchCondition = .always(true)
     let c2: some FetchCondition = .always(true)
-    expectNoDifference((c1 && c2).isSatisfied(in: QueryContext()), true)
+    expectNoDifference((c1 || c2).isSatisfied(in: QueryContext()), true)
   }
 
-  @Test("Is False When Right Condition Is False")
-  func falseWhenRightFalse() {
+  @Test("Is True When Right Condition Is False")
+  func trueWhenRightFalse() {
     let c1: some FetchCondition = .always(true)
     let c2: some FetchCondition = .always(false)
-    expectNoDifference((c1 && c2).isSatisfied(in: QueryContext()), false)
+    expectNoDifference((c1 || c2).isSatisfied(in: QueryContext()), true)
   }
 
-  @Test("Is False When Left Condition Is False")
-  func falseWhenLeftFalse() {
+  @Test("Is True When Left Condition Is False")
+  func trueWhenLeftFalse() {
     let c1: some FetchCondition = .always(false)
     let c2: some FetchCondition = .always(true)
-    expectNoDifference((c1 && c2).isSatisfied(in: QueryContext()), false)
+    expectNoDifference((c1 || c2).isSatisfied(in: QueryContext()), true)
   }
 
   @Test("Is False When Both Conditions Are False")
   func falseWhenBothFalse() {
     let c1: some FetchCondition = .always(false)
     let c2: some FetchCondition = .always(false)
-    expectNoDifference((c1 && c2).isSatisfied(in: QueryContext()), false)
+    expectNoDifference((c1 || c2).isSatisfied(in: QueryContext()), false)
   }
 
-  @Test("&&s Subscribed Values")
-  func andsSubscribedValues() {
+  @Test("||s Subscribed Values")
+  func orsSubscribedValues() {
     let c1 = TestCondition()
     let c2 = TestCondition()
     let values = Lock([Bool]())
-    let subscription = (c1 && c2)
+    let subscription = (c1 || c2)
       .subscribe(in: QueryContext()) { value in
         values.withLock { $0.append(value) }
       }
@@ -45,11 +45,12 @@ struct AndConditionTests {
     c1.send(true)
     c2.send(true)
     c1.send(false)
-    c1.send(true)
     c2.send(false)
+    c1.send(true)
+    c1.send(false)
     c2.send(true)
 
-    values.withLock { expectNoDifference($0, [false, true, false, true, false, true]) }
+    values.withLock { expectNoDifference($0, [true, true, true, false, true, false, true]) }
 
     subscription.cancel()
   }
