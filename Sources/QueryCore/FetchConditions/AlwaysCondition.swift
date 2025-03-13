257c796cc2beed7277654caf167c45099fe8c0ff
mhayes853 <matthewha853@gmail.com>
2025-03-12 17:33:17 -0700

    Inline trivial FetchConditions

--- Sources/QueryCore/FetchConditions/AlwaysCondition.swift
+++ Sources/QueryCore/FetchConditions/AlwaysCondition.swift
@@ -1,20 +1,36 @@
 public struct AlwaysCondition: FetchCondition {
+  @usableFromInline
   let isTrue: Bool
 
+  @usableFromInline
+  let shouldEmitInitialValue: Bool
+
+  @usableFromInline
+  init(isTrue: Bool, shouldEmitInitialValue: Bool) {
+    self.isTrue = isTrue
+    self.shouldEmitInitialValue = shouldEmitInitialValue
+  }
+
+  @inlinable
   public func isSatisfied(in context: QueryContext) -> Bool {
     isTrue
   }
 
+  @inlinable
   public func subscribe(
     in context: QueryContext,
     _ observer: @escaping @Sendable (Bool) -> Void
   ) -> QuerySubscription {
-    .empty
+    if self.shouldEmitInitialValue {
+      observer(self.isTrue)
+    }
+    return .empty
   }
 }
 
 extension FetchCondition where Self == AlwaysCondition {
-  public static func always(_ value: Bool) -> Self {
-    Self(isTrue: value)
+  @inlinable
+  public static func always(_ value: Bool, shouldEmitInitialValue: Bool = false) -> Self {
+    Self(isTrue: value, shouldEmitInitialValue: shouldEmitInitialValue)
   }
 }
--- Sources/QueryCore/FetchConditions/NotCondition.swift
+++ Sources/QueryCore/FetchConditions/NotCondition.swift
@@ -1,20 +1,31 @@
+// MARK: - NotCondition
+
 public struct NotCondition<Base: FetchCondition> {
+  @usableFromInline
   let base: Base
 
-  public func isSatisfied(in context: QueryContext) -> Bool {
-    !base.isSatisfied(in: context)
+  @usableFromInline
+  init(base: Base) {
+    self.base = base
   }
 
+  @inlinable
+  public func isSatisfied(in context: QueryContext) -> Bool {
+    !self.base.isSatisfied(in: context)
+  }
+
+  @inlinable
   public func subscribe(
     in context: QueryContext,
     _ observer: @escaping @Sendable (Bool) -> Void
   ) -> QuerySubscription {
-    base.subscribe(in: context) { satisfied in
-      observer(!satisfied)
-    }
+    self.base.subscribe(in: context) { observer(!$0) }
   }
 }
 
+// MARK: - Not Operator
+
+@inlinable
 public prefix func ! <Base: FetchCondition>(
   _ base: Base
 ) -> NotCondition<Base> {
