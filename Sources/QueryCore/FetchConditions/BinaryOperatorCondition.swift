184b0652623dd323608719f5297eff3502753350
mhayes853 <matthewha853@gmail.com>
2025-03-12 17:18:24 -0700

    Cleanup BinaryOperatorCondition

--- Sources/QueryCore/FetchConditions/BinaryOperatorCondition.swift
+++ Sources/QueryCore/FetchConditions/BinaryOperatorCondition.swift
@@ -1,45 +1,36 @@
+// MARK: - BinaryOperatorConditions
+
 public struct BinaryOperatorCondition<Left: FetchCondition, Right: FetchCondition> {
-  let left: Left
-  let right: Right
-  let op: Operator
+  fileprivate let left: Left
+  fileprivate let right: Right
+  fileprivate let op: Operator
 }
 
-enum Operator {
-  case and
-  case or
-}
+// MARK: - FetchCondition Conformance
 
 extension BinaryOperatorCondition: FetchCondition {
   public func isSatisfied(in context: QueryContext) -> Bool {
-    switch self.op {
-    case .or: self.left.isSatisfied(in: context) || self.right.isSatisfied(in: context)
-    case .and: self.left.isSatisfied(in: context) && self.right.isSatisfied(in: context)
-    }
+    self.op.evaluate(self.left.isSatisfied(in: context), self.right.isSatisfied(in: context))
   }
 
   public func subscribe(
     in context: QueryContext,
     _ observer: @escaping @Sendable (Bool) -> Void
   ) -> QuerySubscription {
+    // TODO: - Flatten nested BinaryOperatorConditions in the lock to improve performance.
     let state = Lock(
       (left: self.left.isSatisfied(in: context), right: self.right.isSatisfied(in: context))
     )
     let s1 = self.left.subscribe(in: context) { value in
       state.withLock {
         $0.left = value
-        switch self.op {
-        case .or: observer($0.left || $0.right)
-        case .and: observer($0.left && $0.right)
-        }
+        observer(self.op.evaluate($0.left, $0.right))
       }
     }
     let s2 = self.right.subscribe(in: context) { value in
       state.withLock {
         $0.right = value
-        switch self.op {
-        case .or: observer($0.left || $0.right)
-        case .and: observer($0.left && $0.right)
-        }
+        observer(self.op.evaluate($0.left, $0.right))
       }
     }
     return QuerySubscription {
@@ -48,6 +39,8 @@ extension BinaryOperatorCondition: FetchCondition {
     }
   }
 }
+
+// MARK: - Operators
 
 public func && <Left: FetchCondition, Right: FetchCondition>(
   _ left: Left,
@@ -61,4 +54,20 @@ public func || <Left: FetchCondition, Right: FetchCondition>(
   _ right: Right
 ) -> BinaryOperatorCondition<Left, Right> {
   BinaryOperatorCondition(left: left, right: right, op: .or)
+}
+
+// MARK: - Operator
+
+private enum Operator {
+  case and
+  case or
+}
+
+extension Operator {
+  func evaluate(_ left: Bool, _ right: Bool) -> Bool {
+    switch self {
+    case .and: left && right
+    case .or: left || right
+    }
+  }
 }
