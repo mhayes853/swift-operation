c3c6b4dc27d295609cf3195c05d55d5b657c44c9
mhayes853 <matthewha853@gmail.com>
2025-03-18 20:11:33 -0700

    Max Retry Index

--- Sources/QueryCore/Queries/RetryQuery.swift
+++ Sources/QueryCore/Queries/RetryQuery.swift
@@ -1,13 +1,46 @@
+import Foundation
+
+// MARK: - BackoffFunction
+
+public struct BackoffFunction: Sendable {
+  private let delay: @Sendable (_ retryIndex: Int) -> TimeInterval
+
+  public init(_ delay: @escaping @Sendable (_ retryIndex: Int) -> TimeInterval) {
+    self.delay = delay
+  }
+}
+
+extension BackoffFunction {
+  public static func linear(delay: TimeInterval) -> Self {
+    Self { TimeInterval($0) * delay }
+  }
+
+  public static func exponential(delay: TimeInterval) -> Self {
+    Self { TimeInterval(pow(delay, Double($0))) }
+  }
+
+  public static let noBackoff = Self { _ in 0 }
+}
+
 // MARK: - RetryModifier
 
 extension QueryProtocol {
-  public func retry(limit: Int) -> ModifiedQuery<Self, some QueryModifier<Self>> {
-    self.modifier(RetryModifier(limit: limit))
+  public func retry(
+    limit: Int,
+    backoff: BackoffFunction
+  ) -> ModifiedQuery<Self, some QueryModifier<Self>> {
+    self.modifier(RetryModifier(limit: limit, backoff: backoff))
   }
 }
 
 private struct RetryModifier<Query: QueryProtocol>: QueryModifier {
   let limit: Int
+  let backoff: BackoffFunction
+
+  func setup(context: inout QueryContext, using query: Query) {
+    context.maxRetryIndex = self.limit
+    query.setup(context: &context)
+  }
 
   func fetch(in context: QueryContext, using query: Query) async throws -> Query.Value {
     var context = context
@@ -33,6 +66,15 @@ extension QueryContext {
   }
 
   private enum RetryIndexKey: Key {
+    static let defaultValue = 0
+  }
+
+  public var maxRetryIndex: Int {
+    get { self[MaxRetryIndexKey.self] }
+    set { self[MaxRetryIndexKey.self] = newValue }
+  }
+
+  private enum MaxRetryIndexKey: Key {
     static let defaultValue = 0
   }
 }
--- Tests/QueryCoreTests/RetryQueryTests.swift
+++ Tests/QueryCoreTests/RetryQueryTests.swift
@@ -6,7 +6,7 @@ import Testing
 struct RetryQueryTests {
   @Test("Zero Retries, Does Not Perform Any Retries")
   func zeroRetriesDoesNotPerformAnyRetries() async {
-    let query = FailingQuery().retry(limit: 0).defaultValue("blob")
+    let query = FailingQuery().retry(limit: 0, backoff: .noBackoff).defaultValue("blob")
     let store = QueryStoreFor<FailingQuery>.detached(query: query)
     await #expect(throws: FailingQuery.SomeError.self) {
       try await store.fetch()
@@ -17,7 +17,7 @@ struct RetryQueryTests {
   func fetchErrorsThenRetriesTheSpecifiedNumberOfTimes() async {
     let query = CountingQuery()
     let store = QueryStoreFor<CountingQuery>
-      .detached(query: query.retry(limit: 3), initialValue: nil)
+      .detached(query: query.retry(limit: 3, backoff: .noBackoff), initialValue: nil)
     await query.ensureFails()
 
     await #expect(throws: CountingQuery.SomeError.self) {
@@ -31,7 +31,7 @@ struct RetryQueryTests {
   func succeedsOnSecondRefetchReturnsValue() async throws {
     let query = SucceedOnNthRefetchQuery(index: 2)
     let store = QueryStoreFor<SucceedOnNthRefetchQuery>
-      .detached(query: query.retry(limit: 3), initialValue: nil)
+      .detached(query: query.retry(limit: 3, backoff: .noBackoff), initialValue: nil)
     let value = try await store.fetch()
     expectNoDifference(value, SucceedOnNthRefetchQuery.value)
   }
@@ -40,8 +40,30 @@ struct RetryQueryTests {
   func succeedsOnFinalRefetchReturnsValue() async throws {
     let query = SucceedOnNthRefetchQuery(index: 3)
     let store = QueryStoreFor<SucceedOnNthRefetchQuery>
-      .detached(query: query.retry(limit: 3), initialValue: nil)
+      .detached(query: query.retry(limit: 3, backoff: .noBackoff), initialValue: nil)
     let value = try await store.fetch()
     expectNoDifference(value, SucceedOnNthRefetchQuery.value)
   }
+
+  @Test("Max Retries Is Based Off Of Limit")
+  func maxRetriesIsBasedOffOfLimit() async throws {
+    let query = FailingQuery().retry(limit: 10, backoff: .noBackoff)
+    let store = QueryStoreFor<FailingQuery>
+      .detached(query: query.retry(limit: 3, backoff: .noBackoff), initialValue: nil)
+    expectNoDifference(store.context.maxRetryIndex, 10)
+  }
+
+  //@Test("Linear Backoff")
+  //func linearBackoff() async throws {
+  //  let query = CountingQuery()
+  //  let store = QueryStoreFor<CountingQuery>
+  //    .detached(query: query.retry(limit: 3, delay: 1), initialValue: nil)
+  //  await query.ensureFails()
+
+  //  await #expect(throws: CountingQuery.SomeError.self) {
+  //    try await store.fetch()
+  //  }
+  //  let count = await query.fetchCount
+  //  expectNoDifference(count, 4)
+  //}
 }
