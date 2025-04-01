8d3094aa290e8d54a91c66e9bf84174834a18470
mhayes853 <matthewha853@gmail.com>
2025-04-01 16:53:49 -0700

    View+queryClient modifier

--- Sources/Query/SwiftUI/EnvironmentValues+QueryClient.swift
+++ Sources/Query/SwiftUI/EnvironmentValues+QueryClient.swift
@@ -1,6 +1,16 @@
 #if canImport(SwiftUI)
   import SwiftUI
 
+  // MARK: - QueryClient Modifier
+
+  extension View {
+    public func queryClient(_ client: QueryClient) -> some View {
+      self.environment(\.queryClient, client)
+    }
+  }
+
+  // MARK: - EnvironmentValues
+
   extension EnvironmentValues {
     public var queryClient: QueryClient {
       get { self[QueryClientKey.self] }
--- Tests/QueryTests/SwiftUITests/State+QueryTests.swift
+++ Tests/QueryTests/SwiftUITests/State+QueryTests.swift
@@ -27,7 +27,7 @@
     func testIsLoading() async throws {
       TestStateQuery.action.withLock { $0 = .suspend }
       let view = QueryView()
-      ViewHosting.host(view: view.environment(\.queryClient, QueryClient()))
+      ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
       let expectation = view.inspection.inspect(after: 0.05) { view in
@@ -38,7 +38,7 @@
 
     func testSuccess() async throws {
       let view = QueryView()
-      ViewHosting.host(view: view.environment(\.queryClient, QueryClient()))
+      ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
       let expectation = view.inspection.inspect(after: 0.05) { view in
@@ -52,7 +52,7 @@
     func testFailure() async throws {
       TestStateQuery.action.withLock { $0 = .fail }
       let view = QueryView()
-      ViewHosting.host(view: view.environment(\.queryClient, QueryClient()))
+      ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
       let expectation = view.inspection.inspect(after: 0.05) { view in
