74f67578053e282687916464d205e9b58c9ca039
mhayes853 <matthewha853@gmail.com>
2025-04-11 11:30:28 -0700

    Docs

--- Sources/Query/AsyncSequences/QueryStore+AsyncSequences.swift
+++ Sources/Query/AsyncSequences/QueryStore+AsyncSequences.swift
@@ -1,4 +1,23 @@
 extension QueryStore {
+  /// An asynchronous sequence of states from a ``QueryStore``.
+  ///
+  /// You can obtain an instance of this sequence by calling ``QueryStore/states``, and use
+  /// for-await-in syntax to iterate over all state updates from the store.
+  ///
+  /// ```swift
+  /// class Observer {
+  ///   init(store: QueryStore<MyQuery.State>) {
+  ///     let task = Task {
+  ///       for await element in store.states {
+  ///         print("State", element.state, element.context)
+  ///       }
+  ///     }
+  ///   }
+  /// }
+  /// ```
+  ///
+  /// For every `AsyncIterator` created with this sequence, a new subscription is added to the
+  /// underlying store.
   public struct AsyncStates: AsyncSequence {
     public struct Element {
       public let state: State
@@ -20,6 +39,24 @@ extension QueryStore {
     }
   }
 
+  /// An asynchronous sequence of states from a ``QueryStore``.
+  ///
+  /// You can use for-await-in syntax to loop over all states from this store.
+  ///
+  /// ```swift
+  /// class Observer {
+  ///   init(store: QueryStore<MyQuery.State>) {
+  ///     let task = Task {
+  ///       for await element in store.states {
+  ///         print("State", element.state, element.context)
+  ///       }
+  ///     }
+  ///   }
+  /// }
+  /// ```
+  ///
+  /// For every `AsyncIterator` created with this sequence, a new subscription is added to the
+  /// underlying store.
   public var states: AsyncStates {
     AsyncStates(store: self)
   }
