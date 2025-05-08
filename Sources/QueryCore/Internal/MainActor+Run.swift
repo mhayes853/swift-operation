125dd2fa01d0551903250bbb67e154df57ffa3dc
mhayes853 <matthewha853@gmail.com>
2025-05-08 12:58:17 -0700

    Cleanup MainActor.runSync

--- Sources/QueryCore/Internal/MainActor+Run.swift
+++ Sources/QueryCore/Internal/MainActor+Run.swift
@@ -2,32 +2,6 @@
   import Foundation
 
   extension MainActor {
-    /// A backport of `MainActor.assumeIsolated`.
-    @_unavailableFromAsync(message:"await the call to the @MainActor closure directly")
-    static func runAssumingIsolation<T: Sendable>(
-      _ operation: @MainActor () throws -> T,
-      file: StaticString = #fileID,
-      line: UInt = #line
-    ) rethrows -> T {
-      #if swift(<5.10)
-        guard Thread.isMainThread else {
-          fatalError(
-            "Incorrect actor executor assumption; Expected same executor as \(self).",
-            file: file,
-            line: line
-          )
-        }
-        // NB: To do the unsafe cast, we have to pretend it's @escaping.
-        return try withoutActuallyEscaping(operation) {
-          try unsafeBitCast($0, to: (() throws -> T).self)()
-        }
-      #else
-        return try assumeIsolated(operation, file: file, line: line)
-      #endif
-    }
-  }
-
-  extension MainActor {
     /// Executes the given body closure on the main actor synchronously.
     ///
     /// - Parameters:
@@ -41,7 +15,7 @@
       line: UInt = #line
     ) rethrows -> T {
       if Thread.isMainThread {
-        try Self.runAssumingIsolation(operation, file: file, line: line)
+        try Self.assumeIsolated(operation, file: file, line: line)
       } else {
         try DispatchQueue.main.sync { try operation() }
       }
