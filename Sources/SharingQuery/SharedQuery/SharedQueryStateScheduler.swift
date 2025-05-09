284eaca0bbc28589a786e9b3321c7e0bf385468a
mhayes853 <matthewha853@gmail.com>
2025-05-09 13:05:32 -0700

    SharedQueryScheduler

--- Sources/SharingQuery/SharedQuery/SharedQueryStateScheduler.swift
+++ Sources/SharingQuery/SharedQuery/SharedQueryStateScheduler.swift
@@ -8,12 +8,17 @@
 
 // MARK: - SharedQueryStateScheduler
 
+/// A protocol to schedule state updates for ``SharedQuery``.
 public protocol SharedQueryStateScheduler: Sendable {
+  /// Schedules the state update.
+  ///
+  /// - Parameter work: The state update.
   func schedule(work: @escaping @Sendable () -> Void)
 }
 
 // MARK: - SynchronousStateScheduler
 
+/// A ``SharedQueryStateScheduler`` that schedules its work synchronously.
 public struct SynchronousStateScheduler: SharedQueryStateScheduler {
   @inlinable
   public func schedule(work: () -> Void) {
@@ -22,12 +27,15 @@ public struct SynchronousStateScheduler: SharedQueryStateScheduler {
 }
 
 extension SharedQueryStateScheduler where Self == SynchronousStateScheduler {
+  /// A ``SharedQueryStateScheduler`` that schedules its work synchronously.
   public static var synchronous: Self { SynchronousStateScheduler() }
 }
 
 // MARK: - AnimationStateScheduler
 
 #if canImport(SwiftUI)
+  /// A ``SharedQueryStateScheduler`` that schedules its work on the MainActor inside a
+  /// `withAnimation` block.
   public struct AnimationStateScheduler: SharedQueryStateScheduler {
     let animation: Animation
 
@@ -39,6 +47,11 @@ extension SharedQueryStateScheduler where Self == SynchronousStateScheduler {
   }
 
   extension SharedQueryStateScheduler where Self == AnimationStateScheduler {
+    /// A ``SharedQueryStateScheduler`` that schedules its work on the MainActor inside a
+    /// `withAnimation` block.
+    ///
+    /// - Parameter animation: The animation to use.
+    /// - Returns: A scheduler.
     public static func animation(_ animation: Animation) -> Self {
       AnimationStateScheduler(animation: animation)
     }
@@ -48,6 +61,8 @@ extension SharedQueryStateScheduler where Self == SynchronousStateScheduler {
 // MARK: - UITransactionStateScheduler
 
 #if SwiftNavigation
+  /// A ``SharedQueryStateScheduler`` that schedules its work on the MainActor inside a
+  /// `withUITransaction` block.
   public struct UITransactionStateScheduler: SharedQueryStateScheduler {
     let transaction: UITransaction
 
@@ -59,6 +74,11 @@ extension SharedQueryStateScheduler where Self == SynchronousStateScheduler {
   }
 
   extension SharedQueryStateScheduler where Self == UITransactionStateScheduler {
+    /// A ``SharedQueryStateScheduler`` that schedules its work on the MainActor inside a
+    /// `withUITransaction` block.
+    ///
+    /// - Parameter transaction: The `UITransaction` to use.
+    /// - Returns: A scheduler.
     public static func transaction(_ transaction: UITransaction) -> Self {
       UITransactionStateScheduler(transaction: transaction)
     }
