b6f2756439398ad47b2d67f7bb3bd0bae67382a9
mhayes853 <matthewha853@gmail.com>
2025-05-11 23:09:14 -0700

    Remove more Task.megaYield

--- Tests/QueryTests/AsyncSequencesTests/AsyncSequenceConditionTests.swift
+++ Tests/QueryTests/AsyncSequencesTests/AsyncSequenceConditionTests.swift
@@ -7,20 +7,26 @@ struct AsyncSequenceConditionTests {
   @Test("Observes Publisher Value")
   func observeSequenceValue() async {
     let (stream, continuation) = AsyncStream<Bool>.makeStream()
+    let (substream, subcontinuation) = AsyncStream<Void>.makeStream()
     let observer: some FetchCondition = .observing(
       sequence: stream,
       initialValue: true
     )
-    let values = RecursiveLock([Bool]())
+    let values = Lock([Bool]())
     let subscription = observer.subscribe(in: QueryContext()) { value in
-      values.withLock { $0.append(value) }
+      values.withLock {
+        $0.append(value)
+        subcontinuation.yield()
+      }
     }
 
+    var subIter = substream.makeAsyncIterator()
+
     continuation.yield(false)
-    await Task.megaYield()
+    await subIter.next()
 
     continuation.yield(true)
-    await Task.megaYield()
+    await subIter.next()
 
     values.withLock { expectNoDifference($0, [false, true]) }
     subscription.cancel()
@@ -30,18 +36,26 @@ struct AsyncSequenceConditionTests {
   func isSequenceValueInContext() async {
     let context = QueryContext()
     let (stream, continuation) = AsyncStream<Bool>.makeStream()
+    let (substream, subcontinuation) = AsyncStream<Void>.makeStream()
     let observer: some FetchCondition = .observing(
       sequence: stream,
       initialValue: true
     )
+    let subscription = observer.subscribe(in: QueryContext()) { _ in
+      subcontinuation.yield()
+    }
     expectNoDifference(observer.isSatisfied(in: context), true)
 
+    var subIter = substream.makeAsyncIterator()
+
     continuation.yield(false)
-    await Task.megaYield()
+    await subIter.next()
     expectNoDifference(observer.isSatisfied(in: context), false)
 
     continuation.yield(true)
-    await Task.megaYield()
+    await subIter.next()
     expectNoDifference(observer.isSatisfied(in: context), true)
+
+    subscription.cancel()
   }
 }
