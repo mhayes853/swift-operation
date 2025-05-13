87231bb4a1b64e41ddab86be8c0f491c77f3711d
mhayes853 <matthewha853@gmail.com>
2025-05-12 17:05:11 -0700

    Increase timeout for State.Query tests

--- Tests/QuerySwiftUITests/State+QueryTests.swift
+++ Tests/QuerySwiftUITests/State+QueryTests.swift
@@ -30,10 +30,10 @@
       ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
-      let expectation = view.inspection.inspect(after: 0.05) { view in
+      let expectation = view.inspection.inspect(after: 0.1) { view in
         XCTAssertNoThrow(try view.find(viewWithId: TestQueryStatusID.loading))
       }
-      await self.fulfillment(of: [expectation], timeout: 0.1)
+      await self.fulfillment(of: [expectation], timeout: 0.2)
     }
 
     func testSuccess() async throws {
@@ -41,12 +41,12 @@
       ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
-      let expectation = view.inspection.inspect(after: 0.05) { view in
+      let expectation = view.inspection.inspect(after: 0.1) { view in
         XCTAssertNoThrow(
           try view.find(viewWithId: TestQueryStatusID.success(TestStateQuery.successValue))
         )
       }
-      await self.fulfillment(of: [expectation], timeout: 0.1)
+      await self.fulfillment(of: [expectation], timeout: 0.2)
     }
 
     func testFailure() async throws {
@@ -55,12 +55,12 @@
       ViewHosting.host(view: view.queryClient(QueryClient()))
 
       try await view.inspection.inspect { try $0.find(button: "Fetch").tap() }
-      let expectation = view.inspection.inspect(after: 0.05) { view in
+      let expectation = view.inspection.inspect(after: 0.1) { view in
         XCTAssertNoThrow(
           try view.find(viewWithId: TestQueryStatusID.error(TestStateQuery.SomeError()))
         )
       }
-      await self.fulfillment(of: [expectation], timeout: 0.1)
+      await self.fulfillment(of: [expectation], timeout: 0.2)
     }
   }
 #endif
