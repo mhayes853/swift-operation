639f4bda1de98c5b3df6e50e54a94aa2daec37d3
mhayes853 <matthewha853@gmail.com>
2025-06-08 19:29:58 -0700

    Update completelyOffline docs

--- Sources/QueryCore/NetworkObserving/SatisfiedConnectionStatusQuery.swift
+++ Sources/QueryCore/NetworkObserving/SatisfiedConnectionStatusQuery.swift
@@ -2,13 +2,14 @@ extension QueryRequest {
   /// Indicates that the query runs completely offline, and therefore does not attempt to make any
   /// network connections.
   ///
+  /// > Warning: Only use this modifier if your query does not attempt to connect to the network in
+  /// > any way shape or form. Otherwise, you'll lose out on benefits such as automatic refetching
+  /// > when the user's network comes back online after being offline.
+  ///
   /// Some queries may not require access to the internet or an external network, and run entirely
   /// locally on the user's device. Examples of this may be running expensive and time-consuming
   /// computations from local file data on a background thread, running an expensive SQL query on a
   /// SQLite database, or streaming data from a locally running LLM.
-  ///
-  /// Using this modifier will prevent your query from being refetched when the user's network
-  /// connection comes back online after being offline.
   ///
   /// - Returns: A ``ModifiedQuery``.
   public func completelyOffline() -> ModifiedQuery<Self, _SatisfiedConnectionStatusModifier<Self>> {
