73038f5c41ce98603b35a911a72e34f60b958de8
mhayes853 <matthewha853@gmail.com>
2025-06-17 17:08:30 -0700

    Update Loggind Docs

--- Sources/QueryCore/Logging/QueryLogger.swift
+++ Sources/QueryCore/Logging/QueryLogger.swift
@@ -2,6 +2,7 @@
   import Logging
 
   extension QueryContext {
+    /// A `Logger` instance used for logging query-related events.
     public var queryLogger: Logger {
       get { self[QueryLoggerKey.self] }
       set { self[QueryLoggerKey.self] = newValue }
