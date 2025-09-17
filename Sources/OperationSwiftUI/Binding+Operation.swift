822b9fa3ebca4ba4e001376835940db9076db67a
mhayes853 <matthewha853@gmail.com>
2025-09-17 16:12:13 -0700

    Cleanup

--- Sources/OperationSwiftUI/Binding+Operation.swift
+++ Sources/OperationSwiftUI/Binding+Operation.swift
@@ -14,29 +14,4 @@
       self = query.$state[HashableStore(store: query.store)]
     }
   }
-
-  // MARK: - HashableStore
-
-  private final class HashableStore<State: OperationState & Sendable>: Hashable {
-    let inner: OperationStore<State>
-
-    init(store: OperationStore<State>) {
-      self.inner = store
-    }
-
-    static func == (lhs: HashableStore<State>, rhs: HashableStore<State>) -> Bool {
-      lhs.inner === rhs.inner
-    }
-
-    func hash(into hasher: inout Hasher) {
-      hasher.combine(ObjectIdentifier(self.inner))
-    }
-  }
-
-  extension OperationState where Self: Sendable {
-    fileprivate subscript(store: HashableStore<Self>) -> Self.StateValue {
-      get { self.currentValue }
-      set { store.inner.currentValue = newValue }
-    }
-  }
 #endif
